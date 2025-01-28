590d582bd18473c5ee9d9fa1fbf226293f286f2e
windyaan <windyanurmaretha@gmail.com>
2025-01-28 18:45:50 +0700

    MainActivity

---
+++ app/src/main/java/com/example/pam_terapi/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.pam_terapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam_terapi.ui.navigation.PengelolaHalaman
+import com.example.pam_terapi.ui.theme.Pam_terapiTheme
+import com.example.praktikum8.ui.viewmodel.PasienApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pam_terapiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PasienApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pam_terapiTheme {
+        Greeting("Android")
+    }
+}
